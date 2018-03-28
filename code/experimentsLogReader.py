3fb76fe6a917853b886d86aee0cd2f6ee791fbff
Janis <s5_steinb_j@venta.lv>
2018-03-28 17:22:27 +0300

    Saving work

--- code/experimentsLogReader.py
+++ code/experimentsLogReader.py
@@ -124,7 +124,6 @@ class ExperimentLogReader():
             header = Scan(self.headerLines)
             header.getParametrs()
             header_source, header_sourceName, header_epoch, header_ra, header_dec, header_timeStart, header_timeStop, header_SystemtemperaturesForScan, header_freqBBC1, header_freqBBC2, header_loa, header_loc, header_clock = header.returnParametrs()
-            print header_freqBBC2
             
             for scan in self.scanLines:
                 scanData = Scan(self.scanLines[scan])
