d8b729404fa5a43147f899eefadb54759eccb3a0
sklandrausis <janis.akmenskalns@gmail.com>
2018-09-04 17:11:52 +0200

    Saving work

--- code/addSpecieToresultJSON.py
+++ code/addSpecieToresultJSON.py
@@ -1,17 +1,17 @@
 import json
+import os
 
 key = "specie"
 value = "ch3oh"
 
-inputFile = "/home/janis/Documents/workspace-sts/DataProcessingForMaserObservation/results/" + "cepa.json"
+inputDir = "/home/janis/Documents/workspace-sts/DataProcessingForMaserObservation/results/" 
 
-
-with open(inputFile) as data_file:
-     data = json.load(data_file)
+for resultFile in os.listdir(inputDir):
+    with open(inputDir + resultFile) as data_file:
+        data = json.load(data_file)
      
-
-for k in data:
-    data[k][key] = value
-
-data_file = open(inputFile, "w")
-data_file.write(json.dumps(data, indent=2))
+    for k in data:
+        data[k][key] = value
+        
+    data_file = open(resultFile, "w")
+    data_file.write(json.dumps(data, indent=2))
