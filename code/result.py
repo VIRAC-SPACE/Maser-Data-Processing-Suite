faf3107eaf83a606dad7d46e0396f740c4ad959d
sklandrausis <janis.akmenskalns@gmail.com>
2018-09-04 15:56:50 +0200

    Monitoring point label display specie

--- code/result.py
+++ code/result.py
@@ -1,11 +1,12 @@
 class Result():
-    def __init__(self, location, date, polarizationU1, polarizationU9, polarizationAVG, iteration_number):
+    def __init__(self, location, date, polarizationU1, polarizationU9, polarizationAVG, iteration_number, specie):
         self.location = location
         self.date = date
         self.polarizationU1 = polarizationU1
         self.polarizationU9 = polarizationU9
         self.polarizationAVG = polarizationAVG
         self.iteration_number = iteration_number
+        self.specie = specie
         
     def __iter__(self):
         yield 'location', self.location
@@ -14,3 +15,4 @@ class Result():
         yield 'polarizationU9', self.polarizationU9
         yield 'polarizationUAVG', self.polarizationAVG
         yield 'iteration_number', self.iteration_number
+        yield 'specie', self.specie
