9f6b56b894a85a8b93ce7f6f7054f461680a16d0
Janis Steinbergs <janis.akmenskalns@gmail.com>
2019-01-04 15:12:39 +0200

    improved skript that removes bad iteration from result json file

--- code/removeIterationFromResult.py
+++ code/removeIterationFromResult.py
@@ -1,4 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import sys
+import os
 import argparse
 import json
 
@@ -21,15 +25,18 @@ def getResult(resultFilePath, source):
     return result
 
 def deleteIteration(result, iteration):
-    tmpKey = ""
+    tmpKey = None
     for key in result.keys():
         if key.endswith("_" + str(iteration)):
             tmpKey = key
         
-    del result[tmpKey]        
+    if tmpKey:
+        del result[tmpKey]
+              
     return result
 
 def removeIterationFromResultJson(resultFilePath, source, updateResult):
+    os.system("cp  " + resultFilePath + source + ".json " + resultFilePath + "/oldresult")
     resultFile = open (resultFilePath + source + ".json", "w")
     resultFile.write(json.dumps(updateResult, indent=2))
     resultFile.close()
