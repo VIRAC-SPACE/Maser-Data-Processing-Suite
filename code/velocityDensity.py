2bef66bae2bcbd3302dd853682a9d7b9a8107f91
Janis Steinbergs <janis.akmenskalns@gmail.com>
2019-01-30 13:30:17 +0200

    optimum solation

--- code/velocityDensity.py
+++ code/velocityDensity.py
@@ -6,6 +6,7 @@ import os
 import argparse
 import numpy as np
 from scipy.integrate import trapz
+from scipy import optimize, signal
 import matplotlib.pyplot as plt
 import json
 from parsers._configparser import ConfigParser
@@ -15,6 +16,7 @@ def parseArguments():
     parser = argparse.ArgumentParser(description='''Velocity Density. ''', epilog="""Velocity Density.""")
     parser.add_argument("-c", "--config", help="Configuration cfg file", type=str, default="config/config.cfg")
     parser.add_argument("-v","--version", action="version", version='%(prog)s - Version 0.1')
+    parser.add_argument("-o", "--output", help="Plot fits for outputfile ")
     parser.add_argument("source", help="Experiment source", type=str, default="")
     args = parser.parse_args()
     return args
@@ -53,6 +55,14 @@ def getData(outputFiles):
         itarationList.append(iteration)
     return (velocitiesList, amplitudeList, itarationList)
 
+def getData2(outputFile):
+    outputDir = getConfigs('paths', 'outputFilePath')
+    file = outputDir + outputFile
+    data = np.fromfile(file, dtype="float64", count=-1, sep=" ").reshape((file_len(file),4))
+    velocity = data[:, [0]]
+    ampvid = data[:, [3]]
+    return (velocity, ampvid)
+
 def addAreaToResultFile(source, iteration, areas):
     resultDir = getConfigs('paths', 'resultFilePath')
     with open(resultDir + source + ".json") as resultData:
@@ -67,38 +77,65 @@ def addAreaToResultFile(source, iteration, areas):
     resultFile.close()
     
 def gauss(x, A, μ, σ):
-    return A / (σ * np.sqrt(2 * np.pi)) * np.exp(-(x-μ)**2 / (2*σ**2))
+    return float(A) / (float(σ) * np.sqrt(2 * np.pi)) * np.exp(-(x-float(μ))**2 / (2*float(σ)**2))
+
+def createGaussFit(velocity, vel, amplitude, sigma):
+    #index = (np.abs(velocity - float(vel))).argmin()
+    g = gauss(velocity, amplitude, float(vel), sigma)
+    return g
   
 def computeDensity(velocity, ampvid, source_velocities, source, iteration):
     velocity = np.array([v[0] for v in velocity])
     ampvid = np.array([a[0] for a in ampvid])
     dx = np.abs(np.abs(velocity[-1]) - np.abs(velocity[0]))/velocity.size
     areas = list()
-    #colors = ['b', 'g', 'c', 'm', 'y', 'k', 'w']
-    #fig = plt.figure()
-    #plt.plot(velocity, ampvid, "r")
     i = 0
     for vel in source_velocities:
-        index = (np.abs(velocity - float(vel))).argmin()
-        g = gauss(velocity, ampvid[index], float(vel), 0.1)
-        #plt.plot(velocity, g, "--" + colors[i], label="--" + colors[i])
+        g = createGaussFit(velocity, vel, ampvid)
         area = trapz(g, dx=dx)
         areas.append(area)
         addAreaToResultFile(source, iteration, areas)
         i = i + 1
+            
+def plotDensity(velocity, amplitude, source_velocities):
+    colors = ['b', 'g', 'c', 'm', 'y', 'k', 'w']
+    fig = plt.figure("Gauss fit")
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('Velocity (km sec$^{-1}$)')
+    ax.set_ylabel('Flux density (Jy)')
+    plt.plot(velocity, amplitude, "r", label="spectre")
+    i = 0
+    def cost(parameters):
+        a, b, c = parameters
+        return np.sum(np.power(gauss(velocity, a, b, c) - amplitude, 2)) / velocity.size
+    for vel in source_velocities:
+        index = (np.abs(velocity - float(vel))).argmin()
+        initial_guess = [float(vel), amplitude[index][0], 0.38]
+        result = optimize.minimize(cost, initial_guess)
+        print("result", result.x, initial_guess)
+        g = createGaussFit(velocity, result.x[0], result.x[1], result.x[2])
+        plt.plot(velocity, g, "--" + colors[i], label=float(vel))
+        i = i + 1
         
-    #plt.legend()
-    #plt.show()
-    #print ("area", areas)
+    plt.grid()    
+    plt.legend()
+    plt.show()
 
 def main():
     args = parseArguments()
+    output = str(args.__dict__["output"])
     source = str(args.__dict__["source"])
-    velocitiesList, amplitudeList, itarationList = getData(getOputFiles(source))
     source_velocities = getConfigs('velocities', source).split(",")
     
-    for i in range(0, len(velocitiesList)):
-        computeDensity(velocitiesList[i], amplitudeList[i], source_velocities, source, itarationList[i])
+    if output == None:
+        velocitiesList, amplitudeList, itarationList = getData(getOputFiles(source))
+        
+        for i in range(0, len(velocitiesList)):
+            computeDensity(velocitiesList[i], amplitudeList[i], source_velocities, source, itarationList[i])
+    else:
+        velocity, amplitude = getData2(output)
+        plotDensity(velocity, amplitude, source_velocities)
+        
     sys.exit(0)
     
 if __name__ == "__main__":
