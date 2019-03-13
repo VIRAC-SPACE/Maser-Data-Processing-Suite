d227fe9be4dac0d30b39649f31e84f5b25e4b6bc
Janis Steinbergs <janis.akmenskalns@gmail.com>
2019-03-13 10:50:26 +0200

    add crossCorComp.py script

--- code/crossCorComp.py
+++ code/crossCorComp.py
@@ -1,18 +1,53 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = [0, 1, 2, 4]
-tfull = [0, 1, 2, 3, 4, 5, 6, 7]
+from help import *
+from parsers._configparser import ConfigParser
 
-x1 = [1, -3, 0.5, 4]
-x2 = [-6, 1,5, 4, 1]
+def parseArguments():
+    parser = argparse.ArgumentParser(description='''Cross - Correlatet two maser componets. ''', epilog="""CrossCorr.""")
+    parser.add_argument("source", help="Experiment source", type=str, default="")
+    parser.add_argument("a", help="componet A ", type=int)
+    parser.add_argument("b", help="componet B", type=str)
+    parser.add_argument("-c", "--config", help="Configuration cfg file", type=str, default="config/config.cfg")
+    parser.add_argument("-v","--version", action="version", version='%(prog)s - Version 2.0')
+    args = parser.parse_args()
+    return args
 
-y_same = np.correlate(x1, x2, "same")
-y_full = np.correlate(x1, x2, "full")
+def getArgs(key):
+    return str(parseArguments().__dict__[key])
 
-print ("y_same", y_same)
-print("y_full", y_full)
+def getConfigs(key, value):
+    configFilePath = getArgs("config")
+    config = ConfigParser.getInstance()
+    config.CreateConfig(configFilePath)
+    return config.getConfig(key, value)
 
-plt.plot(t, x1)
-plt.plot(tfull, y_full)
-plt.show()
+def getData():
+    monitoringFile = "monitoring/" + getArgs("source") + ".txt"
+    a = int(getArgs("a"))
+    b = int(getArgs("b"))
+    compunetCount = len(getConfigs("velocities", getArgs("source")).replace(" ", "").split(","))
+    data = np.fromfile(monitoringFile, dtype="float64", count=-1, sep=" ").reshape((file_len(monitoringFile),compunetCount + 1))    
+    x = correctNumpyReadData(data[:, [a]])
+    y = correctNumpyReadData(data[:, [b]])
+    return (x, y)
+
+def main():
+    x = getData()[0]
+    y = getData()[1]
+    crossCorr = np.correlate(x, y, "full")
+    plt.plot(crossCorr)
+    plt.grid(True)
+    plt.show()
+    sys.exit(0)
+    
+if __name__ == "__main__":
+    main()
+    
