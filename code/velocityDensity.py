f017c9a47ab8dff25bc871370cf595d14694e27b
Sklandrausis <janis.akmenskalns@gmail.com>
2019-04-23 13:36:34 +0300

    add gauss fit

--- code/velocityDensity.py
+++ code/velocityDensity.py
@@ -1,18 +1,13 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-import os
 import argparse
 import numpy as np
-from scipy.integrate import trapz
-from scipy import optimize
 import matplotlib.pyplot as plt
-from lmfit import models
-import json
-from multiprocessing import Process
-import threading
+from astropy.modeling import models, fitting
+from astropy.modeling.fitting import LevMarLSQFitter
 from functools import reduce
+
 from parsers._configparser import ConfigParser
 from help import *
 
@@ -35,201 +30,40 @@ def getConfigs(key, value):
     config.CreateConfig(configFilePath)
     return config.getConfig(key, value)
 
-def addAreaToResultFileProcess(source, iteration, areas):
-    resultDir = getConfigs('paths', 'resultFilePath')
-    with open(resultDir + source + ".json") as resultData:
-        results = json.load(resultData)
-         
-    for experiment in results:
-        if experiment.endswith("_" + str(iteration)):
-            results[experiment]["areas"] = areas
-                 
-    resultFile = open (resultDir +  source + ".json", "w")
-    resultFile.write(json.dumps(results, indent=2))
-    resultFile.close()
-
-def addAreaToResultFile(source, iteration, areas):
-    p = Process(target=addAreaToResultFileProcess, args=(source, iteration, areas))
-    p.start()
-    p.join()
-
-class DataFileProcessing(threading.Thread):
-    def __init__(self, source):
-        __slots__ = ['source', 'outputDir']
-        super().__init__()
-        threading.Thread.__init__(self) 
-        self.source = source
-        self.outputDir = getConfigs('paths', 'outputFilePath')
-        self.velocitiesList = list()
-        self.amplitudeList = list()
-        self.itarationList = list()
-        
-    def getOputFiles(self):
-        outputFileList = [outputFile for  outputFile in   os.listdir(self.outputDir) if outputFile.startswith(self.source)]
-        return outputFileList
-    
-    def getDataForSingleDataFile(self, outputFile):
-        file = self.outputDir + outputFile
-        data = np.fromfile(file, dtype="float64", count=-1, sep=" ").reshape((file_len(file),4))
-        velocity = data[:, [0]]
-        ampvid = data[:, [3]]
-        return (velocity, ampvid)
-    
-    def getDataForAllDataFile(self):
-        for outputFile in self.getOputFiles():
-            velocity, ampvid = self.getDataForSingleDataFile(outputFile)
-            self.velocitiesList.append(velocity)
-            self.amplitudeList.append(ampvid)
-            iteration = outputFile.split(".")[0].split("_")[-1]
-            self.itarationList.append(iteration)
-            
-    def getData(self):
-        return (self.velocitiesList, self.amplitudeList, self.itarationList)
-        
-    def run(self):
-        self.getDataForAllDataFile()
-
-class VelocityDensity(threading.Thread):
-    def __init__(self, velocity, amplitude, source, itaration):
-        __slots__ = ['velocity', 'amplitude', 'source_velocities', 'source', 'itaration', 'areasList']
-        super().__init__()
-        threading.Thread.__init__(self) 
-        self.velocity = np.array([v[0] for v in velocity])
-        self.amplitude = np.array([a[0] for a in amplitude])
-        self.source = source
-        self.source_velocities = getConfigs('velocities', self.source).split(",")
-        self.itaration = itaration
-        self.areas = list()
-        
-    def gauss(self, A, μ, σ):
-        return float(A) / (float(σ) * np.sqrt(2 * np.pi)) * np.exp(-(self.velocity-float(μ))**2 / (2*float(σ)**2))
-    
-    def createGaussFit(self, vel, A, sigma):
-        return  self.gauss(A, float(vel), sigma)
-    
-    def cost(self,parameters):
-        a, b, c = parameters
-        return np.sum(np.power(self.gauss(a, b, c) - self.amplitude, 2)) / self.velocity.size
-    
-    def computeDensity(self):
-        dx = np.abs(np.abs(self.velocity[-1]) - np.abs(self.velocity[0]))/self.velocity.size
-        
-        i = 0        
-        for vel in self.source_velocities:
-            index = (np.abs(self.velocity - float(vel))).argmin()
-            initial_guess = [float(vel), self.amplitude[index], 0.38]
-            result = optimize.minimize(self.cost, initial_guess)
-            g = self.createGaussFit(result.x[0], result.x[1], result.x[2])
-            area = trapz(g, dx=dx)
-            self.areas.append(area)
-            i = i + 1
-                    
-    def write(self):
-        addAreaToResultFile(self.source, self.itaration, self.areas)
-        
-    def run(self):
-        self.computeDensity()
-                
-class VelocityDensityPolter(VelocityDensity):
-    def __init__(self, velocity, amplitude, source):
-        self.velocity = np.array([v[0] for v in velocity])
-        self.amplitude = np.array([a[0] for a in amplitude])
-        self.source = source
-        self.source_velocities = getConfigs('velocities', self.source).split(",")
-        
-    def createModel(self):
-        modelList = list()
-        paramsList = list()
-        prefix = self.source + "_"
-        
-        def sum(a,b):
-            return a + b
-        i = 0
-        j = 1
-        
-        cuts = getConfigs('cuts', self.source).split(";")
-        cuts = [c.split(",") for c in  cuts]
-        cutsIntervalCount = len(cuts) // 2
-        
-        indexFirst = (np.abs(self.velocity - float(cuts[0][0]))).argmin()    
-        indexLast = (np.abs(self.velocity - float(cuts[0][1]))).argmin()
-        x = self.velocity[indexFirst:indexLast]
-        y = self.amplitude[indexFirst:indexLast]
-        dx = x - np.mean(x)
-        fwhm = 2 * np.sqrt(np.sum((dx * dx) * y) / np.sum(y))
-        sigmaN = fwhm / 2.355
-        
-        print("sigmaN", sigmaN)
-        
-        for component in range (0, len(self.source_velocities)):
-            model_n = models.GaussianModel(prefix=prefix + str(component))
-            modelList.append(model_n)
-            if component == 4:
-                sigma = (np.abs(float(self.source_velocities[-1])))/6
-            else:
-                sigma = (np.abs(float(self.source_velocities[i]) - float(self.source_velocities[j])))/6
-                
-            print("sigma", sigma)
-            params_n = model_n.make_params(center=float(self.source_velocities[component]), sigma=sigmaN)
-            paramsList.append(params_n)
-            i = i + 1
-            j = j + 1
-        
-        model = reduce(sum, modelList)
-        param = reduce(sum, paramsList)
-        
-        output = model.fit(self.amplitude, param, x=self.velocity)
-        fig, gridspec = output.plot(data_kws={'markersize': 1})
-        plt.show()
-             
-    def plotDensity(self):
-        colors = ['b', 'g', 'c', 'm', 'y', 'k', 'w']
-        fig = plt.figure("Gauss fit")
-        ax = fig.add_subplot(111)
-        ax.set_xlabel('Velocity (km sec$^{-1}$)')
-        ax.set_ylabel('Flux density (Jy)')
-        plt.plot(self.velocity, self.amplitude, "r", label="spectre")
-        i = 0
-        
-        for vel in self.source_velocities:
-            index = (np.abs(self.velocity - float(vel))).argmin()
-            initial_guess = [float(vel), self.amplitude[index], 0.38]
-            result = optimize.minimize(self.cost, initial_guess)
-            g = self.createGaussFit(result.x[0], result.x[1], result.x[2])
-            plt.plot(self.velocity, g, "--" + colors[i], label=float(vel))
-            i = i + 1
-            
-        plt.grid()    
-        plt.legend()
-        plt.show()
-        
-def main():
-    args = parseArguments()
-    output = str(args.__dict__["output"])
-    source = str(args.__dict__["source"])
-    
-    dataFileProcessing = DataFileProcessing(source)
-    
-    if output == "":
-        dataFileProcessing.start()
-        dataFileProcessing.join()
-        velocitiesList, amplitudeList, itarationList = dataFileProcessing.getData()
-        
-        for i in range(0, len(velocitiesList)):
-            velocityDensity = VelocityDensity(velocitiesList[i], amplitudeList[i], source, itarationList[i])
-            velocityDensity.start()
-            velocityDensity.join()
-            velocityDensity.write()
-                    
-    else:
-        velocity, amplitude = dataFileProcessing.getDataForSingleDataFile(output)
-        velocityDensityPolter = VelocityDensityPolter(velocity, amplitude, source)
-        #velocityDensityPolter.plotDensity()
-        velocityDensityPolter.createModel()
-         
-    sys.exit(0)
-    
 if __name__ == "__main__":
-    p = Process(target=main)
-    p.start()
-    
+    gaussLines = getConfigs("gauss_lines", getArgs("source")).replace(" ", "").split(",")
+    dataFile = getConfigs("paths", "notSmoohtFilePath") + getArgs("source") + "/" + getArgs("output")
+
+    data = np.fromfile(dataFile, dtype="float64", count=-1, sep=" ").reshape((file_len(dataFile), 4))
+    velocity = correctNumpyReadData(data[:, [0]])
+    ampvid = correctNumpyReadData(data[:, [3]])
+    indexies = [(np.abs(velocity - float(line))).argmin() for line in gaussLines]
+    mons = [max(ampvid[index-5:index+5]) for index in indexies]
+    gaussian = [models.Gaussian1D(mons[index], gaussLines[index], 0.05, bounds={'stddev': (None, 0.15)}) for index in range(0, len(mons))]
+
+    def sum(a,b):
+        return a + b
+    gg_init = reduce(sum, gaussian)
+
+    fitter = fitting.SLSQPLSQFitter()
+    fit = LevMarLSQFitter()
+    gg_fit = fit(gg_init, velocity, ampvid)
+    print(gg_fit.parameters)
+    sts = [models.Gaussian1D(gg_fit[index].amplitude, gg_fit[index].mean, gg_fit[index].stddev) for index in range(0, len(gaussLines))]
+
+    plt.plot(velocity, ampvid, 'b+', label="data")
+    plt.plot(velocity, gg_fit(velocity), "-", label="total fit", linewidth=4,)
+
+    colors = ["b", "g", "r", "c", "m", "y", "k", "w","C1", "C2", "C3", "C4"]
+    c = 0
+    for st in sts:
+        plt.plot(velocity, st(velocity), colors[c], label="ST" + str(c))
+        c += 1
+
+    plt.plot(velocity, (gg_fit(velocity) - ampvid) * 1 - 35)
+    plt.plot(velocity, velocity * 0 - 35)
+    plt.legend()
+    plt.show()
+
+    print("Fit value",  np.mean(ampvid - gg_fit(velocity)))
+
