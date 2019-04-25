59927a2f146007a67a59971a31f2187d9c65413c
Sklandrausis <janis.akmenskalns@gmail.com>
2019-04-25 09:53:59 +0300

    add gausian areas to result json file

--- code/velocityDensity.py
+++ code/velocityDensity.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os,sys
 import argparse
 import numpy as np
 from numpy import trapz
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting
 from astropy.modeling.fitting import LevMarLSQFitter
 from functools import reduce
+import json
 
 from parsers._configparser import ConfigParser
 from help import *
@@ -31,50 +33,80 @@ def getConfigs(key, value):
     config.CreateConfig(configFilePath)
     return config.getConfig(key, value)
 
-if __name__ == "__main__":
+def iterationFromDataFile(dataFileName):
+    return dataFileName.split(".")[0].split("_")[-1]
+
+def computeGauss(file):
     gaussLines = getConfigs("gauss_lines", getArgs("source")).replace(" ", "").split(",")
-    dataFile = getConfigs("paths", "notSmoohtFilePath") + getArgs("source") + "/" + getArgs("output")
+    dataFile = getConfigs("paths", "notSmoohtFilePath") + getArgs("source") + "/" + file  #getArgs("output")
 
     data = np.fromfile(dataFile, dtype="float64", count=-1, sep=" ").reshape((file_len(dataFile), 4))
     velocity = correctNumpyReadData(data[:, [0]])
     ampvid = correctNumpyReadData(data[:, [3]])
     indexies = [(np.abs(velocity - float(line))).argmin() for line in gaussLines]
-    mons = [max(ampvid[index-5:index+5]) for index in indexies]
+    mons = [max(ampvid[index - 5:index + 5]) for index in indexies]
     gaussian = [models.Gaussian1D(mons[index], gaussLines[index], 0.05, bounds={'stddev': (None, 0.15)}) for index in range(0, len(mons))]
 
-    def sum(a,b):
+    def sum(a, b):
         return a + b
-    gg_init = reduce(sum, gaussian)
 
+    gg_init = reduce(sum, gaussian)
     fitter = fitting.SLSQPLSQFitter()
     fit = LevMarLSQFitter()
     gg_fit = fit(gg_init, velocity, ampvid)
-    print(gg_fit.parameters)
     sts = [models.Gaussian1D(gg_fit[index].amplitude, gg_fit[index].mean, gg_fit[index].stddev) for index in range(0, len(gaussLines))]
+    gaussianAreas = []
 
-
-    plt.figure("Gausian fits")
-    plt.plot(velocity, ampvid, 'C0+', label="data")
-    plt.plot(velocity, gg_fit(velocity), "C1-", label="total fit", linewidth=4)
-
-    colors = []
-
-    for index in range(2, len(gaussLines) + 2):
-        if index %2:
-            index -= 1
-        else:
-            index += 1
-        colors.append("C" + str(index))
-
-    c = 0
     for st in sts:
-        print("Area of st" + str(c), trapz(st(velocity), velocity))
-        plt.plot(velocity, st(velocity), colors[c], label="ST" + str(c))
-        c += 1
+        gaussianAreas.append(trapz(st(velocity), velocity))
 
-    plt.plot(velocity, (gg_fit(velocity) - ampvid) * 1 - 35)
-    plt.plot(velocity, velocity * 0 - 35)
-    plt.legend()
-    plt.show()
+    return (gaussianAreas, sts, gg_fit, velocity, ampvid, gaussLines)
 
-    print("Fit value",  np.mean(ampvid - gg_fit(velocity)))
+def addAreasToResultFiles(file, gaussianAreas):
+    with open(getConfigs("paths", "resultFilePath") + getArgs("source") + ".json", "r") as resultFile:
+        results = json.load(resultFile)
+
+    for experiment in results:
+        if int(results[experiment]["Iteration_number"]) == int(iterationFromDataFile(file)):
+            results[experiment]["areas"] = gaussianAreas
+
+    with open(getConfigs("paths", "resultFilePath") + getArgs("source") + ".json", "w") as resultFile:
+        resultFile.write(json.dumps(results, indent=2))
+
+if __name__ == "__main__":
+    if getArgs("output") != "":
+        gaussianAreas, sts, gg_fit, velocity, ampvid, gaussLines = computeGauss(getArgs("output"))
+
+        plt.figure("Gausian fits")
+        plt.plot(velocity, ampvid, 'C0+', label="data")
+        plt.plot(velocity, gg_fit(velocity), "C1-", label="total fit", linewidth=4)
+
+        colors = []
+
+        for index in range(2, len(gaussLines) + 2):
+            if index %2:
+                index -= 1
+            else:
+                index += 1
+            colors.append("C" + str(index))
+
+        c = 0
+        for st in sts:
+            plt.plot(velocity, st(velocity), colors[c], label="ST" + str(c))
+            c += 1
+
+        plt.plot(velocity, (gg_fit(velocity) - ampvid) * 1 - 35)
+        plt.plot(velocity, velocity * 0 - 35)
+        plt.legend()
+        plt.show()
+
+        print("Fit value",  np.mean(ampvid - gg_fit(velocity)))
+
+        addAreasToResultFiles(getArgs("output"), gaussianAreas)
+
+    else:
+        for resultFile in os.listdir(getConfigs("paths", "notSmoohtFilePath") + getArgs("source")):
+            print("processing file", resultFile)
+            addAreasToResultFiles(resultFile, computeGauss(resultFile)[0])
+
+    sys.exit(0)
