7cc10e84d4f5814b20a436d4add0df18b5bdfb34
Sklandrausis <janis.akmenskalns@gmail.com>
2019-07-15 14:29:47 +0300

    save

--- code/simpleM.py
+++ code/simpleM.py
@@ -1,25 +1,63 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+from datetime import datetime
 
 from help import *
 from parsers._configparser import ConfigParser
 
-def getConfigs(key, value):
-    configFilePath = "config/config.cfg"
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='''Monitoring velocity amplitudes in time. ''', epilog="""Monitor.""")
+    parser.add_argument("source", help="Source Name", type=str)
+    parser.add_argument("-c", "--config", help="Configuration cfg file", type=str, default="config/config.cfg")
+    parser.add_argument("-v", "--version", action="version", version='%(prog)s - Version 2.0')
+    args = parser.parse_args()
+    return args
+
+
+def get_args(key):
+    args = parse_arguments()
+    return str(args.__dict__[key])
+
+
+def get_configs(key, value):
+    config_file_path = get_args("config")
     config = ConfigParser.getInstance()
-    config.CreateConfig(configFilePath)
+    config.CreateConfig(config_file_path)
     return config.getConfig(key, value)
 
-components = [1,2,3,4,5]
-file = "monitoring/cepa.txt"
-compunetCount = len(getConfigs("velocities", "cepa").replace(" ", "").split(","))
-velocity = getConfigs("velocities", "cepa").replace(" ", "").split(",")
-data = np.fromfile(file, dtype="float64", count=-1, sep=" ").reshape((file_len(file),compunetCount + 1))
-x = correctNumpyReadData(data[:, [0]])
 
-for component in components:
-    index = components.index(component)
-    plt.plot(x,correctNumpyReadData(data[:, [index +1]]), label=velocity[index])
-    plt.legend()
-    plt.grid(True)
-plt.show()
+def main():
+    old_monitoring_file = "old_monitoring/" + get_args("source") + ".dat"
+    new_monitoring_file = "monitoring/" + get_args("source") + ".txt"
+    compunet_count = len(get_configs("velocities", get_args("source") + "_6668").replace(" ", "").split(","))
+    velocity = get_configs("velocities", get_args("source") + "_6668").replace(" ", "").split(",")
+    old_data = np.loadtxt(old_monitoring_file, dtype=str).reshape((file_len(old_monitoring_file), compunet_count + 1))
+    new_data = np.fromfile(new_monitoring_file, dtype="float64", count=-1, sep=" ").reshape((file_len(new_monitoring_file), compunet_count + 1))
+    new_x = correctNumpyReadData(new_data[:, [0]])
+    old_x = correctNumpyReadData(old_data[:, [0]])
+    old_x = [convertDatetimeObjectToMJD(datetime.strptime(x, "%Y-%m-%d%H:%M:%S")) for x in old_x]
+    x = old_x + list(new_x)
+    components = [i for i in range(1, compunet_count + 1)]
+    old_data[:, [0]] = old_x[0]
+
+    data = np.array(np.concatenate((old_data, new_data), axis=0), dtype="float64")
+    print(data)
+
+    for component in components:
+        index = components.index(component)
+        plt.plot(x, correctNumpyReadData(data[:, [index + 1]]), label=velocity[index])
+        plt.legend()
+        plt.grid(True)
+    plt.show()
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
