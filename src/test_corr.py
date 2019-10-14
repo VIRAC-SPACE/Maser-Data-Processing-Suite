7c29d2cc93a503cae2c55dfa3f7d775516d6648e
Sklandrausis <janis.akmenskalns@gmail.com>
2019-10-14 16:23:04 +0300

    save

--- src/test_corr.py
+++ src/test_corr.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 from scipy.signal import correlate
 from scipy import signal
 import pycwt as wavelet
+import random
 
-t = np.arange(0, 100, 0.1)
+t = np.arange(0, 1e5, 0.1)
 
-a1 = 1
-a2 = 2
+r = random.random()
+print("r", r)
 
-f1 = 0.5
-f2 = 0.6
+a1 = 20
+a2 = 19
+
+f1 = 1234.0
+f2 = 1234.0
 
 p1 = 0
 p2 = 0
@@ -47,13 +51,41 @@ plt.ylabel('Cross-correlation between components s1 and s2')
 plt.grid(True)
 
 fig3 = plt.figure("coherence")
-f, c_xy = signal.coherence(s1, s2, nfft=256)
-plt.semilogy(f, c_xy, label="scipy_signal_coherence")
+f, c_xy = signal.coherence(s1, s2, nfft=1024, nperseg=1024)
+f2, c_xy2 = signal.coherence(s1, s2, nfft=1024, nperseg=1024)
+plt.semilogy(f, c_xy, label="scipy_signal_coherence_1")
+plt.semilogy(f2, c_xy2, label="scipy_signal_coherence")
 plt.xlabel('frequency')
 plt.ylabel('Coherence')
-plt.cohere(s1, s2, NFFT=256, label="pyplot_cohere")
+
 plt.legend()
 plt.grid(True)
+plt.show()
+
+
+fs = 10e3
+print("fs", fs)
+N = 1e5
+amp = 20
+freq = 1234.0
+noise_power = 0.001 * fs / 2
+time = np.arange(N) / fs
+b, a = signal.butter(2, 0.25, 'low')
+x = np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
+y = signal.lfilter(b, a, x)
+x += amp*np.sin(2*np.pi*freq*time)
+y += np.random.normal(scale=0.1*np.sqrt(noise_power), size=time.shape)
+
+f, Cxy = signal.coherence(x, y, fs, nperseg=1024)
+fig4 = plt.figure("coh 2")
+plt.semilogy(f, Cxy)
+plt.xlabel('frequency [Hz]')
+plt.ylabel('Coherence')
+
+fig5 = plt.figure("coh time")
+plt.plot(x)
+plt.plot(y)
+plt.xlabel('time')
 plt.show()
 
 
