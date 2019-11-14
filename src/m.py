e7498f33d8bd3126f6829e748d85f84ad0329e17
Sklandrausis <janis.akmenskalns@gmail.com>
2019-11-14 15:56:39 +0200

    add m.py

--- src/m.py
+++ src/m.py
@@ -64,12 +64,10 @@ def main():
     symbols = ["*-", "o-", "v-", "^-", "<-", ">-", "1-", "2-", "3-", "4-"]
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
-
     f, (ax1, ax2) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [2, 4]}, dpi=75, figsize=(20, 12))
     f.tight_layout(pad=2, h_pad=2, w_pad=2, rect=None)
     f.suptitle(get_configs("Full_source_name", get_args("source")), horizontalalignment="center", verticalalignment="center", x=0.4)
 
-    #ax1 = plt.subplot(121)
     specter_files = ["cepa_23_53_48_22_Dec_2018_IRBENE16_294.dat", "cepa_22_24_38_23_Jul_2018_IRBENE16_46.dat", "cepa_22_08_44_29_Aug_2018_IRBENE16_121.dat"]
 
     symbols_2 = [".", "-.", "-"]
@@ -85,11 +83,11 @@ def main():
         ax1.plot(x_, y_, symbols_2[i])
         i += 1
 
+    ax1.set_xlim(-5.5, -1)
     ax1.set_xlabel("Velocity (km sec$^{-1}$)")
     ax1.set_ylabel("Flux density (Jy)")
 
 
-    #ax2 = plt.subplot(122)
     for component in components:
         index = components.index(component)
         y = correctNumpyReadData(data[:, [index + 1]])
