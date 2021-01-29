291e203596b515427e002dbf8e06f2541674ff0e
sklandrausis <janis.akmenskalns@gmail.com>
2021-01-29 14:55:44 +0200

    save work

--- publications_and_presentations/parallax_data.py
+++ publications_and_presentations/parallax_data.py
@@ -1,7 +1,7 @@
-from functools import reduce
-
 import sys
 import os
+from collections import namedtuple
+from functools import reduce
 import argparse
 from datetime import datetime
 
@@ -39,59 +39,53 @@ def get_configs_items(section):
     return config.get_items(section)
 
 
+def get_maser_short_name(source):
+    all_sources_short_name = list(get_configs_items("Full_source_name").keys())
+    all_sources_full_name = list(get_configs_items("Full_source_name").values())
+    try:
+        source_short_name = all_sources_short_name[all_sources_full_name.index(source)]
+    except ValueError:
+        source_short_name = "*"
+        print(source, " is not in config file")
+
+    return source_short_name
+
+
 def main(infile):
     data = ascii.read(infile)
     sources = data["Source"]
-    flux = data["Flux"]
-    cflux = data["Cflux"]
     distance = data["Distance"]
-    x_ = np.array([float(data["X"][i]) for i in range(0, len(data["X"])) if data["X"][i] != "*"])
-    y_ = np.array([float(data["Y"][i]) for i in range(0, len(data["Y"])) if data["Y"][i] != "*"])
-    flux = np.array([float(flux[i]) for i in range(0, len(flux)) if data["X"][i] != "*"])
-    cflux = np.array([float(cflux[i]) for i in range(0, len(cflux)) if data["Y"][i] != "*"])
-    sources = [sources[i] for i in range(0, len(sources)) if data["Y"][i] != "*"]
-    distance = [float(distance[i]) for i in range(0, len(distance)) if data["Y"][i] != "*"]
+    x = data["X"]
+    y = data["Y"]
+
+    Maser = namedtuple('Maser', 'long_name short_name x y flux distance '
+                                'fluctuation_indexes variability_indexes mean_of_y absolute_mean_of_y, outlier')
+
+    masers = [Maser(sources[i], get_maser_short_name(sources[i]), x[i], y[i], [], distance[i], [], [], [], [],
+                    outlier=False) for i in range(0, len(sources))]
 
     old_monitoring_file_path = get_configs("paths", "oldMonitoringFilePath")
     new_monitoring_file_path = get_configs("paths", "monitoringFilePath")
 
-    all_sources_short_name = list(get_configs_items("Full_source_name").keys())
-    all_sources_full_name = list(get_configs_items("Full_source_name").values())
+    for maser in masers:
+        print("Executing for maser with short name", maser.short_name, "long name", maser.long_name)
 
-    fluctuation_indexes = dict()
-    variability_indexes = dict()
-    mean_of_y = dict()
-    absolute_mean_of_y = dict()
-    distance_ = dict()
-    outliers = []
+        if maser.short_name != "*":
+            old = False
+            new = False
+            both = False
 
-    for source in sources:
-        old = False
-        new = False
-        both = False
-        print(source)
-        try:
-            source_short_name = all_sources_short_name[all_sources_full_name.index(source)]
-        except ValueError:
-            print(source, " is not in config file")
-        else:
-
-            new_monitoring_file = new_monitoring_file_path + "/" + source_short_name + "_" + "6668" + ".npy"
-            old_monitoring_file = old_monitoring_file_path + "/" + source_short_name + ".dat"
-            fluctuation_indexes[source_short_name] = []
-            variability_indexes[source_short_name] = []
-            mean_of_y[source_short_name] = []
-            absolute_mean_of_y[source_short_name] = []
-            distance_[source_short_name] = distance[sources.index(source)]
+            new_monitoring_file = new_monitoring_file_path + "/" + maser.short_name + "_" + "6668" + ".npy"
+            old_monitoring_file = old_monitoring_file_path + "/" + maser.short_name + ".dat"
 
             new_data = None
             old_data = None
             monitoring_data = None
-
             if os.path.isfile(old_monitoring_file) and os.path.isfile(new_monitoring_file):
-                source_velocities = get_configs('velocities', source_short_name + "_" + "6668").split(",")
+                source_velocities = get_configs('velocities', maser.short_name + "_" + "6668").split(",")
                 source_velocities = [si.strip() for si in source_velocities]
-                component_count = len(get_configs("velocities", source_short_name + "_" + "6668").replace(" ", "").split(","))
+                component_count = len(
+                    get_configs("velocities", maser.short_name + "_" + "6668").replace(" ", "").split(","))
                 components = [i for i in range(1, component_count + 1)]
                 new_data = np.load(new_monitoring_file, allow_pickle=True)
                 new_x = new_data[0][0]
@@ -121,9 +115,10 @@ def main(infile):
                 both = True
 
             elif os.path.isfile(old_monitoring_file) and not os.path.isfile(new_monitoring_file):
-                source_velocities = get_configs('velocities', source_short_name + "_" + "6668").split(",")
+                source_velocities = get_configs('velocities', maser.short_name + "_" + "6668").split(",")
                 source_velocities = [si.strip() for si in source_velocities]
-                component_count = len(get_configs("velocities", source_short_name + "_" + "6668").replace(" ", "").split(","))
+                component_count = len(
+                    get_configs("velocities", maser.short_name + "_" + "6668").replace(" ", "").split(","))
                 components = [i for i in range(1, component_count + 1)]
                 old_data = np.loadtxt(old_monitoring_file, dtype=str).reshape(
                     (file_len(old_monitoring_file), component_count + 1))
@@ -135,9 +130,10 @@ def main(infile):
                 old = True
 
             elif not os.path.isfile(old_monitoring_file) and os.path.isfile(new_monitoring_file):
-                source_velocities = get_configs('velocities', source_short_name + "_" + "6668").split(",")
+                source_velocities = get_configs('velocities', maser.short_name + "_" + "6668").split(",")
                 source_velocities = [si.strip() for si in source_velocities]
-                component_count = len(get_configs("velocities", source_short_name + "_" + "6668").replace(" ", "").split(","))
+                component_count = len(
+                    get_configs("velocities", maser.short_name + "_" + "6668").replace(" ", "").split(","))
                 components = [i for i in range(1, component_count + 1)]
                 new_data = np.load(new_monitoring_file, allow_pickle=True)
                 monitoring_data = new_data
@@ -146,7 +142,7 @@ def main(infile):
             else:
                 components = []
                 source_velocities = []
-                monitoring_data = None
+                data = None
 
             if len(components) > 0:
                 if monitoring_data is not None:
@@ -160,35 +156,105 @@ def main(infile):
                             y_data = monitoring_data[index + 1]
                         y_data = np.array([np.float128(yi) for yi in y_data]).clip(min=0)
                         N = len(y_data)
-                        variability_index = ((np.max(y_data) - np.std(y_data)) - (np.min(y_data) + np.std(y_data))) / \
-                                            ((np.max(y_data) - np.std(y_data)) + (np.min(y_data) + np.std(y_data)))
-                        fluctuation_index = np.sqrt((N / reduce(lambda x__, y__: x__ + y__, [(1.5 + 0.05 * i) ** 2 for i in y_data])) *
+                        variability_index = ((np.max(y_data) - np.std(y_data)) -
+                                             (np.min(y_data) + np.std(y_data))) / \
+                                            ((np.max(y_data) - np.std(y_data)) +
+                                             (np.min(y_data) + np.std(y_data)))
+
+                        fluctuation_index = np.sqrt((N / reduce(lambda x__, y__: x__ + y__,
+                                                                [(1.5 + 0.05 * i) ** 2 for i in y_data])) *
                                                     ((reduce(lambda x__, y__: x__ + y__,
-                                                             [i ** 2 * (1.5 + 0.05 * i) ** 2 for i in y_data]) - np.mean(y_data) *
-                                                      reduce(lambda x__, y__: x__ + y__, [i * (1.5 + 0.05 * i) ** 2 for i in y_data]))
-                                                     / (N - 1)) - 1) / np.mean(y_data)
+                                                             [i ** 2 * (1.5 + 0.05 * i) ** 2 for i in y_data]) -
+                                                      np.mean(y_data) * reduce(lambda x__, y__: x__ + y__,
+                                                                               [i * (1.5 + 0.05 * i) ** 2 for i
+                                                                                in y_data])) / (N - 1)) - 1) / \
+                                            np.mean(y_data)
                         if not np.isnan(fluctuation_index):
-                            fluctuation_indexes[source_short_name].append(np.float64(variability_index))
-                            variability_indexes[source_short_name].append(np.float64(fluctuation_index))
-                            mean_of_y[source_short_name].append(np.float64(np.mean(y_data)))
-                            distance_tmp = distance[sources.index(source)]
-                            absolute_mean_of_y[source_short_name].append(np.float64(np.mean(y_data)) * (distance_tmp/2)**2)
-                            if fluctuation_index > 1:
-                                source_name = get_configs("Full_source_name", source_short_name)
-                                outliers.append(
-                                    [variability_index, fluctuation_index, source_name + " " + source_velocities[index]])
+                            maser.variability_indexes.append(np.float64(variability_index))
+                            maser.fluctuation_indexes.append(np.float64(fluctuation_index))
+                            maser.mean_of_y.append(np.float64(np.mean(y_data)))
+                            maser.flux.extend(y_data)
+                            if maser.distance != "*":
+                                maser.absolute_mean_of_y.append(np.float64(np.mean(y_data)) *
+                                                                (np.float64(maser.distance) / 2) ** 2)
+
+                            #if fluctuation_index > 1:
+                                #maser.outlier = True
 
                         del y_data, variability_index, fluctuation_index
                     del monitoring_data
-            del new_monitoring_file, old_monitoring_file
+                del new_monitoring_file, old_monitoring_file
 
-    fluctuation_indexes_sizes = []
-    variability_indexes_sizes = []
-    fluctuation_indexes_vs_variability_indexes_sizes = []
-    fluctuation_indexes_vs_variability_indexes_sizes_absolute = []
-    fluctuation_indexes_vs_variability_color = []
-    mean_of_y_ = []
+    fig1, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
+    fig2, ax2 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
+    fig3, ax3 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
+    fig4, ax4 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
+    fig5, ax5 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
+    fig6, ax6 = plt.subplots(nrows=1, ncols=1, figsize=(16, 16), dpi=150)
 
+    axs = [ax1, ax2, ax3, ax4]
+    for ax in axs:
+        ax.set_xlabel("X [Kpc]")
+        ax.set_ylabel("Y [Kpc]")
+
+    ax5.set_xlabel("Variability indexes")
+    ax5.set_ylabel("Fluctuation indexes")
+    ax5.set_title("Variability indexes vs Fluctuation indexes")
+
+    ax6.set_xlabel("Distance [Kpc]")
+    ax6.set_ylabel("Flux density (Jy)")
+    ax6.set_title("Flux vs Distance")
+
+    for maser in masers:
+        if maser.x != "*" and maser.y != "*":
+            ax1.scatter(float(maser.x), float(maser.y), s=np.mean(maser.mean_of_y))
+            ax1.set_title("Flux")
+
+            ax2.scatter(float(maser.x), float(maser.y), s=np.mean(maser.absolute_mean_of_y))
+            ax2.set_title("CFlux")
+
+            ax3.scatter(float(maser.x), float(maser.y), s=100 * np.array(np.mean(maser.fluctuation_indexes)))
+            ax3.set_title("Fluctuation indexes")
+
+            ax4.scatter(float(maser.x), float(maser.y), s=100 * np.array(np.mean(maser.variability_indexes)))
+            ax4.set_title("Variability indexes")
+
+        collor1 = []
+        size1 = []
+        variability_indexes = maser.variability_indexes
+        means_y = maser.mean_of_y
+        for vi in variability_indexes:
+            if vi < 0.5:
+                collor1.append("blue")
+            else:
+                collor1.append("red")
+
+        for my in means_y:
+            if 0.5 < my <= 20:
+                ss = 100
+            elif 20 < my <= 200:
+                ss = 200
+            elif 200 < my <= 800:
+               ss = 300
+            elif 800 < my <= 2000:
+                ss = 400
+            elif 2000 < my <= 3005:
+                ss = 500
+            else:
+                ss = 600
+            size1.append(ss)
+
+            if maser.distance != "*":
+                ax6.scatter(float(maser.distance), my, s=ss)
+        ax5.scatter(maser.variability_indexes, maser.fluctuation_indexes, c=collor1, s=size1)
+
+
+        #s=fluctuation_indexes_vs_variability_indexes_sizes, c=fluctuation_indexes_vs_variability_color
+
+
+
+
+    '''
     for source in sources:
         try:
             source_short_name = all_sources_short_name[all_sources_full_name.index(source)]
@@ -206,18 +272,7 @@ def main(infile):
                 fluctuation_indexes_vs_variability_color.append("red")
 
             my = np.mean(mean_of_y[source_short_name])
-            if 0.5 < my <= 20:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(100)
-            elif 20 < my <= 200:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(200)
-            elif 200 < my <= 800:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(300)
-            elif 800 < my <= 2000:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(400)
-            elif 2000 < my <= 3005:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(500)
-            else:
-                fluctuation_indexes_vs_variability_indexes_sizes.append(600)
+            
 
             my = np.mean(absolute_mean_of_y[source_short_name])
             if 0.5 < my <= 20:
@@ -234,59 +289,13 @@ def main(infile):
                 fluctuation_indexes_vs_variability_indexes_sizes_absolute.append(600)
 
     plt.figure(dpi=150)
-    plt.scatter(x_, y_, s=0.1 * flux)
-    plt.xlim(min(x_) - 0.3, max(x_) + 0.3)
-    plt.ylim(min(y_) - 0.3, max(y_) + 0.3)
-    plt.xlabel("X [Kpc]")
-    plt.ylabel("Y [Kpc]")
-    plt.title("Flux")
-
-    plt.figure(dpi=150)
-    plt.scatter(x_, y_, s=0.1 * cflux)
-    plt.xlim(min(x_) - 0.3, max(x_) + 0.3)
-    plt.ylim(min(y_) - 0.3, max(y_) + 0.3)
-    plt.xlabel("X [Kpc]")
-    plt.ylabel("Y [Kpc]")
-    plt.title("CFlux")
-
-    plt.figure(dpi=150)
-    plt.scatter(x_, y_, s=100 * np.array(fluctuation_indexes_sizes))
-    plt.xlim(min(x_) - 0.3, max(x_) + 0.3)
-    plt.ylim(min(y_) - 0.3, max(y_) + 0.3)
-    plt.xlabel("X [Kpc]")
-    plt.ylabel("Y [Kpc]")
-    plt.title("Fluctuation indexes")
-
-    plt.figure(dpi=150)
-    plt.scatter(x_, y_, s=100 * np.array(variability_indexes_sizes))
-    plt.xlim(min(x_) - 0.3, max(x_) + 0.3)
-    plt.ylim(min(y_) - 0.3, max(y_) + 0.3)
-    plt.xlabel("X [Kpc]")
-    plt.ylabel("Y [Kpc]")
-    plt.title("Variability indexes")
-
-    plt.figure(dpi=150)
-    plt.scatter(variability_indexes_sizes, fluctuation_indexes_sizes,
-                s=fluctuation_indexes_vs_variability_indexes_sizes, c=fluctuation_indexes_vs_variability_color)
-    plt.xlabel("Variability indexes")
-    plt.ylabel("Fluctuation indexes")
-    plt.title("Variability indexes vs Fluctuation indexes")
-
-    plt.figure(dpi=150)
-    plt.scatter(distance, mean_of_y_,
-                s=fluctuation_indexes_vs_variability_indexes_sizes, c=fluctuation_indexes_vs_variability_color)
-    plt.xlabel("Distance [Kpc]")
-    plt.ylabel("Flux density (Jy)")
-    plt.title("Flux vs Distance")
-
-  
-    plt.figure(dpi=150)
     plt.scatter(variability_indexes_sizes, fluctuation_indexes_sizes,
                 s=fluctuation_indexes_vs_variability_indexes_sizes_absolute, c=fluctuation_indexes_vs_variability_color)
     plt.xlabel("Variability indexes")
     plt.ylabel("Fluctuation indexes")
     plt.title("Variability indexes vs Fluctuation indexes")
-
+    
+    '''
     plt.show()
 
 
