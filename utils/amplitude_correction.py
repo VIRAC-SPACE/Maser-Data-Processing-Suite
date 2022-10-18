e40429bc3f16f3db53abb974779ee1039384f005
sklandrausis <janis.akmenskalns@gmail.com>
2022-10-18 12:34:11 +0300

    save work

--- utils/amplitude_correction.py
+++ utils/amplitude_correction.py
@@ -5,6 +5,13 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+PACKAGE_PARENT = '..'
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+
 from parsers.configparser_ import ConfigParser
 from utils.help import find_nearest_index
 
